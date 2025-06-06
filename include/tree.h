46298213731421c3718e1cc205264976f52a85aa
DisraeliOG <118463303+DisraeliOG@users.noreply.github.com>
2025-06-06 16:06:45 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,27 +2,26 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
-#pragma once
 #include <memory>
 #include <vector>
 
 class PMTree {
- public:
-  char value;
-  std::vector<std::shared_ptr<PMTree>> children;
+public:
+    char value;
+    std::vector<std::shared_ptr<PMTree>> children;
 
-  PMTree() = default;
-  PMTree(const std::vector<char>& elements);
-  PMTree(const std::vector<char>& elements, const std::vector<char>& path);
+    PMTree() = default;
+    explicit PMTree(const std::vector<char>& elements);
+    PMTree(const std::vector<char>& elements, const std::vector<char>& path);
 
-  void collectPermutations(std::vector<std::vector<char>>& perms,
-                           std::vector<char>& current);
-  std::vector<char> getPerm1(int& counter, int target);
-  std::vector<char> getPerm2(int target, int& factorialLevel) const;
-  int factorial(int n) const;
+    void collectPermutations(std::vector<std::vector<char>>& perms,
+                             std::vector<char>& current);
+    std::vector<char> getPerm1(int& counter, int target);
+    std::vector<char> getPerm2(int target, int& factorialLevel) const;
+    int factorial(int n) const;
 
- private:
-  void build(const std::vector<char>& elements, const std::vector<char>& path);
+private:
+    void build(const std::vector<char>& elements, const std::vector<char>& path);
 };
 
 std::vector<std::vector<char>> getAllPerms(PMTree& tree);
